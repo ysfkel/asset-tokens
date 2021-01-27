ec840cc655b1758dad2d898270971aa8de279924
ysfkel <yusufkel@gmail.com>
2021-01-27 13:10:43 +0400

    updated migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
-var MyToken = artifacts.require("GLDToken.sol")
+var GLDToken = artifacts.require("GLDToken.sol")
+const GLDCrowdSale = artifacts.require("GLDCrowdSale.sol")
+require("dotenv").config({path: "../.env"}) 
 
 module.exports = async function(deployer) {
-    await deployer.deploy(MyToken, 1000000)
+    let addr = await web3.eth.getAccounts();
+    await deployer.deploy(GLDToken, INITIAL_TOKENS)
+    await deployer.deploy(GLDCrowdSale, 1, addr[0], GLDToken.address)
+    let instance = await GLDToken.deployed();
+        // transfer all tokens to GLDCrowdSale contract
+    await instance.transfer(GLDCrowdSale.address, INITIAL_TOKENS)
 }
